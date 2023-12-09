4319248ce2810c210ad7352a7929ce22436a86dd
Navaneetha Krishnan <149750760+krishna-1912@users.noreply.github.com>
2023-12-09 20:29:58 +0530

    Update DS_Phone_Pulse_Data_Visualization_and_Exploration.py

--- DS_Phone_Pulse_Data_Visualization_and_Exploration.py
+++ DS_Phone_Pulse_Data_Visualization_and_Exploration.py
@@ -1,73 +1,68 @@
 import json
 import streamlit as st
+import plotly.express as px
+import plotly.graph_objects as go
 import pandas as pd
 import requests
 import mysql.connector
-import plotly.express as px
-import plotly.graph_objects as go
 
 # mysql connection
-DATABASE = 'phonepe_data'
-mydb = mysql.connector.connect(
+try:
+    # Your database connection and query code here
+    mydb = mysql.connector.connect(
     host="localhost",
     port=3306,
     user="root",
     password="root",
-    database=DATABASE
-)
+    database="phonepe_data"
+    )
+except Exception as e:
+    print(f"Unable to connect to database: {e}")
+
 cursor = mydb.cursor()
 
-#Aggregated_transsaction
-cursor.execute("select * from aggregated_transaction;")
-mydb.commit()
-table1 = cursor.fetchall()
-Aggre_trans = pd.DataFrame(table1,columns = ("States", "Years", "Quarter", "Transaction_type", "Transaction_count", "Transaction_amount"))
+# fetch aggregated_transsaction
+cursor.execute("select * from aggregated_transaction")
+aggr_trans_table = cursor.fetchall()
+aggre_trans = pd.DataFrame(aggr_trans_table, columns = ("States", "Years", "Quarter", "Transaction_type", "Transaction_count", "Transaction_amount"))
 
-#Aggregated_user
+# fetch aggregated_user
 cursor.execute("select * from aggregated_user")
-mydb.commit()
-table2 = cursor.fetchall()
-Aggre_user = pd.DataFrame(table2,columns = ("States", "Years", "Quarter", "Brands", "Transaction_count", "Percentage"))
+aggr_user_table = cursor.fetchall()
+aggre_user = pd.DataFrame(aggr_user_table, columns = ("States", "Years", "Quarter", "Brands", "Transaction_count", "Percentage"))
 
-#Map_transaction
+# fetch map_transaction
 cursor.execute("select * from map_transaction")
-mydb.commit()
-table3 = cursor.fetchall()
-Map_trans = pd.DataFrame(table3,columns = ("States", "Years", "Quarter", "Districts", "Transaction_count", "Transaction_amount"))
+map_trans_table = cursor.fetchall()
+map_trans = pd.DataFrame(map_trans_table, columns = ("States", "Years", "Quarter", "Districts", "Transaction_count", "Transaction_amount"))
 
-#Map_user
+# fetch map_user
 cursor.execute("select * from map_user")
-mydb.commit()
-table4 = cursor.fetchall()
-Map_user = pd.DataFrame(table4,columns = ("States", "Years", "Quarter", "Districts", "RegisteredUser", "AppOpens"))
+map_user_table = cursor.fetchall()
+map_user = pd.DataFrame(map_user_table,columns = ("States", "Years", "Quarter", "Districts", "RegisteredUser", "AppOpens"))
 
-#Top_transaction
+# fetch top_transaction
 cursor.execute("select * from top_transaction")
-mydb.commit()
-table5 = cursor.fetchall()
-Top_trans = pd.DataFrame(table5,columns = ("States", "Years", "Quarter", "Pincodes", "Transaction_count", "Transaction_amount"))
+top_trans_table = cursor.fetchall()
+top_trans = pd.DataFrame(top_trans_table,columns = ("States", "Years", "Quarter", "Pincodes", "Transaction_count", "Transaction_amount"))
 
-#Top_user
+# fetch top_user
 cursor.execute("select * from top_user")
-mydb.commit()
-table6 = cursor.fetchall()
-Top_user = pd.DataFrame(table6, columns = ("States", "Years", "Quarter", "Pincodes", "RegisteredUser"))
+top_user_table = cursor.fetchall()
+top_user = pd.DataFrame(top_user_table, columns = ("States", "Years", "Quarter", "Pincodes", "RegisteredUser"))
 
 def animate_all_amount():
     url = "https://gist.githubusercontent.com/jbrobst/56c13bbbf9d97d187fea01ca62ea5112/raw/e388c4cae20aa53cb5090210a42ebb9b765c0a36/india_states.geojson"
-    response =requests.get(url)
+    response = requests.get(url)
     data1 = json.loads(response.content)
     state_names_tra = [feature["properties"]["ST_NM"] for feature in data1["features"]]
     state_names_tra.sort()
 
     df_state_names_tra = pd.DataFrame({"States":state_names_tra})
-
     frames = []
-
-    for year in Map_user["Years"].unique():
-        for quarter in Aggre_trans["Quarter"].unique():
-
-            at1 = Aggre_trans[(Aggre_trans["Years"]==year)&(Aggre_trans["Quarter"]==quarter)]
+    for year in map_user["Years"].unique():
+        for quarter in aggre_trans["Quarter"].unique():
+            at1 = aggre_trans[(aggre_trans["Years"]==year)&(aggre_trans["Quarter"]==quarter)]
             atf1 = at1[["States","Transaction_amount"]]
             atf1 = atf1.sort_values(by="States")
             atf1["Years"]=year
@@ -75,7 +70,6 @@ def animate_all_amount():
             frames.append(atf1)
 
     merged_df = pd.concat(frames)
-
     fig_tra = px.choropleth(merged_df, geojson= data1, locations= "States", featureidkey= "properties.ST_NM", color= "Transaction_amount",
                             color_continuous_scale= "Sunsetdark", range_color= (0,4000000000), hover_name= "States", title = "TRANSACTION AMOUNT",
                             animation_frame="Years", animation_group="Quarter")
@@ -85,15 +79,6 @@ def animate_all_amount():
     fig_tra.update_layout(title_font= {"size":25})
     return st.plotly_chart(fig_tra)
 
-def payment_count():
-    attype= Aggre_trans[["Transaction_type", "Transaction_count"]]
-    att1= attype.groupby("Transaction_type")["Transaction_count"].sum()
-    df_att1= pd.DataFrame(att1).reset_index()
-    fig_pc= px.bar(df_att1,x= "Transaction_type",y= "Transaction_count",title= "TRANSACTION TYPE and TRANSACTION COUNT",
-                color_discrete_sequence=px.colors.sequential.Redor_r)
-    fig_pc.update_layout(width=600, height= 500)
-    return st.plotly_chart(fig_pc)
-
 def animate_all_count():
     url = "https://gist.githubusercontent.com/jbrobst/56c13bbbf9d97d187fea01ca62ea5112/raw/e388c4cae20aa53cb5090210a42ebb9b765c0a36/india_states.geojson"
     response= requests.get(url)
@@ -102,14 +87,11 @@ def animate_all_count():
     state_names_tra.sort()
 
     df_state_names_tra= pd.DataFrame({"States":state_names_tra})
-
-
     frames= []
+    for year in aggre_trans["Years"].unique():
+        for quarter in aggre_trans["Quarter"].unique():
 
-    for year in Aggre_trans["Years"].unique():
-        for quarter in Aggre_trans["Quarter"].unique():
-
-            at1= Aggre_trans[(Aggre_trans["Years"]==year)&(Aggre_trans["Quarter"]==quarter)]
+            at1= aggre_trans[(aggre_trans["Years"]==year)&(aggre_trans["Quarter"]==quarter)]
             atf1= at1[["States", "Transaction_count"]]
             atf1=atf1.sort_values(by="States")
             atf1["Years"]=year
@@ -127,8 +109,17 @@ def animate_all_count():
     fig_tra.update_layout(title_font={"size":25})
     return st.plotly_chart(fig_tra)
 
+def payment_count():
+    attype= aggre_trans[["Transaction_type", "Transaction_count"]]
+    att1= attype.groupby("Transaction_type")["Transaction_count"].sum()
+    df_att1= pd.DataFrame(att1).reset_index()
+    fig_pc= px.bar(df_att1,x= "Transaction_type",y= "Transaction_count",title= "TRANSACTION TYPE and TRANSACTION COUNT",
+                color_discrete_sequence=px.colors.sequential.Redor_r)
+    fig_pc.update_layout(width=600, height= 500)
+    return st.plotly_chart(fig_pc)
+
 def payment_amount():
-    attype= Aggre_trans[["Transaction_type","Transaction_amount"]]
+    attype= aggre_trans[["Transaction_type","Transaction_amount"]]
     att1= attype.groupby("Transaction_type")["Transaction_amount"].sum()
     df_att1= pd.DataFrame(att1).reset_index()
     fig_tra_pa= px.bar(df_att1, x= "Transaction_type", y= "Transaction_amount", title= "TRANSACTION TYPE and TRANSACTION AMOUNT",
@@ -137,7 +128,7 @@ def payment_amount():
     return st.plotly_chart(fig_tra_pa)
 
 def reg_all_states(state):
-    mu= Map_user[["States","Districts","RegisteredUser"]]
+    mu= map_user[["States","Districts","RegisteredUser"]]
     mu1= mu.loc[(mu["States"]==state)]
     mu2= mu1[["Districts", "RegisteredUser"]]
     mu3= mu2.groupby("Districts")["RegisteredUser"].sum()
@@ -155,8 +146,8 @@ def transaction_amount_year(sel_year):
     state_names_tra.sort()
 
     year= int(sel_year)
-    atay= Aggre_trans[["States","Years","Transaction_amount"]]
-    atay1= atay.loc[(Aggre_trans["Years"]==year)]
+    atay= aggre_trans[["States","Years","Transaction_amount"]]
+    atay1= atay.loc[(aggre_trans["Years"]==year)]
     atay2= atay1.groupby("States")["Transaction_amount"].sum()
     atay3= pd.DataFrame(atay2).reset_index()
 
@@ -171,8 +162,8 @@ def transaction_amount_year(sel_year):
 
 def payment_count_year(sel_year):
     year= int(sel_year)
-    apc= Aggre_trans[["Transaction_type", "Years", "Transaction_count"]]
-    apc1= apc.loc[(Aggre_trans["Years"]==year)]
+    apc= aggre_trans[["Transaction_type", "Years", "Transaction_count"]]
+    apc1= apc.loc[(aggre_trans["Years"]==year)]
     apc2= apc1.groupby("Transaction_type")["Transaction_count"].sum()
     apc3= pd.DataFrame(apc2).reset_index()
 
@@ -180,7 +171,6 @@ def payment_count_year(sel_year):
                     color_discrete_sequence=px.colors.sequential.Brwnyl_r)
     fig_apc.update_layout(width=600, height=500)
     return st.plotly_chart(fig_apc)
-
 
 def transaction_count_year(sel_year):
     url= "https://gist.githubusercontent.com/jbrobst/56c13bbbf9d97d187fea01ca62ea5112/raw/e388c4cae20aa53cb5090210a42ebb9b765c0a36/india_states.geojson"
@@ -190,8 +180,8 @@ def transaction_count_year(sel_year):
     state_names_tra.sort()
 
     year= int(sel_year)
-    atcy= Aggre_trans[["States", "Years", "Transaction_count"]]
-    atcy1= atcy.loc[(Aggre_trans["Years"]==year)]
+    atcy= aggre_trans[["States", "Years", "Transaction_count"]]
+    atcy1= atcy.loc[(aggre_trans["Years"]==year)]
     atcy2= atcy1.groupby("States")["Transaction_count"].sum()
     atcy3= pd.DataFrame(atcy2).reset_index()
 
@@ -203,11 +193,10 @@ def transaction_count_year(sel_year):
     fig_atcy.update_layout(title_font={"size":25})
     return st.plotly_chart(fig_atcy)
 
-
 def payment_amount_year(sel_year):
     year= int(sel_year)
-    apay = Aggre_trans[["Years", "Transaction_type", "Transaction_amount"]]
-    apay1= apay.loc[(Aggre_trans["Years"]==year)]
+    apay = aggre_trans[["Years", "Transaction_type", "Transaction_amount"]]
+    apay1= apay.loc[(aggre_trans["Years"]==year)]
     apay2= apay1.groupby("Transaction_type")["Transaction_amount"].sum()
     apay3= pd.DataFrame(apay2).reset_index()
 
@@ -218,8 +207,8 @@ def payment_amount_year(sel_year):
 
 def reg_state_all_RU(sel_year,state):
     year= int(sel_year)
-    mus= Map_user[["States", "Years", "Districts", "RegisteredUser"]]
-    mus1= mus.loc[(Map_user["States"]==state)&(Map_user["Years"]==year)]
+    mus= map_user[["States", "Years", "Districts", "RegisteredUser"]]
+    mus1= mus.loc[(map_user["States"]==state)&(map_user["Years"]==year)]
     mus2= mus1.groupby("Districts")["RegisteredUser"].sum()
     mus3= pd.DataFrame(mus2).reset_index()
 
@@ -230,8 +219,8 @@ def reg_state_all_RU(sel_year,state):
 
 def reg_state_all_TA(sel_year,state):
     year= int(sel_year)
-    mts= Map_trans[["States", "Years","Districts", "Transaction_amount"]]
-    mts1= mts.loc[(Map_trans["States"]==state)&(Map_trans["Years"]==year)]
+    mts= map_trans[["States", "Years","Districts", "Transaction_amount"]]
+    mts1= mts.loc[(map_trans["States"]==state)&(map_trans["Years"]==year)]
     mts2= mts1.groupby("Districts")["Transaction_amount"].sum()
     mts3= pd.DataFrame(mts2).reset_index()
 
@@ -241,7 +230,7 @@ def reg_state_all_TA(sel_year,state):
     return st.plotly_chart(fig_mts)
 
 def ques1():
-    brand= Aggre_user[["Brands","Transaction_count"]]
+    brand= aggre_user[["Brands","Transaction_count"]]
     brand1= brand.groupby("Brands")["Transaction_count"].sum().sort_values(ascending=False)
     brand2= pd.DataFrame(brand1).reset_index()
 
@@ -250,7 +239,7 @@ def ques1():
     return st.plotly_chart(fig_brands)
 
 def ques2():
-    lt= Aggre_trans[["States", "Transaction_amount"]]
+    lt= aggre_trans[["States", "Transaction_amount"]]
     lt1= lt.groupby("States")["Transaction_amount"].sum().sort_values(ascending= True)
     lt2= pd.DataFrame(lt1).reset_index().head(10)
 
@@ -259,7 +248,7 @@ def ques2():
     return st.plotly_chart(fig_lts)
 
 def ques3():
-    htd= Map_trans[["Districts", "Transaction_amount"]]
+    htd= map_trans[["Districts", "Transaction_amount"]]
     htd1= htd.groupby("Districts")["Transaction_amount"].sum().sort_values(ascending=False)
     htd2= pd.DataFrame(htd1).head(10).reset_index()
 
@@ -268,7 +257,7 @@ def ques3():
     return st.plotly_chart(fig_htd)
 
 def ques4():
-    htd= Map_trans[["Districts", "Transaction_amount"]]
+    htd= map_trans[["Districts", "Transaction_amount"]]
     htd1= htd.groupby("Districts")["Transaction_amount"].sum().sort_values(ascending=True)
     htd2= pd.DataFrame(htd1).head(10).reset_index()
 
@@ -276,9 +265,8 @@ def ques4():
                     color_discrete_sequence=px.colors.sequential.Greens_r)
     return st.plotly_chart(fig_htd)
 
-
 def ques5():
-    sa= Map_user[["States", "AppOpens"]]
+    sa= map_user[["States", "AppOpens"]]
     sa1= sa.groupby("States")["AppOpens"].sum().sort_values(ascending=False)
     sa2= pd.DataFrame(sa1).reset_index().head(10)
 
@@ -287,7 +275,7 @@ def ques5():
     return st.plotly_chart(fig_sa)
 
 def ques6():
-    sa= Map_user[["States", "AppOpens"]]
+    sa= map_user[["States", "AppOpens"]]
     sa1= sa.groupby("States")["AppOpens"].sum().sort_values(ascending=True)
     sa2= pd.DataFrame(sa1).reset_index().head(10)
 
@@ -296,7 +284,7 @@ def ques6():
     return st.plotly_chart(fig_sa)
 
 def ques7():
-    stc= Aggre_trans[["States", "Transaction_count"]]
+    stc= aggre_trans[["States", "Transaction_count"]]
     stc1= stc.groupby("States")["Transaction_count"].sum().sort_values(ascending=True)
     stc2= pd.DataFrame(stc1).reset_index()
 
@@ -305,7 +293,7 @@ def ques7():
     return st.plotly_chart(fig_stc)
 
 def ques8():
-    stc= Aggre_trans[["States", "Transaction_count"]]
+    stc= aggre_trans[["States", "Transaction_count"]]
     stc1= stc.groupby("States")["Transaction_count"].sum().sort_values(ascending=False)
     stc2= pd.DataFrame(stc1).reset_index()
 
@@ -314,7 +302,7 @@ def ques8():
     return st.plotly_chart(fig_stc)
 
 def ques9():
-    ht= Aggre_trans[["States", "Transaction_amount"]]
+    ht= aggre_trans[["States", "Transaction_amount"]]
     ht1= ht.groupby("States")["Transaction_amount"].sum().sort_values(ascending= False)
     ht2= pd.DataFrame(ht1).reset_index().head(10)
 
@@ -323,7 +311,7 @@ def ques9():
     return st.plotly_chart(fig_lts)
 
 def ques10():
-    dt= Map_trans[["Districts", "Transaction_amount"]]
+    dt= map_trans[["Districts", "Transaction_amount"]]
     dt1= dt.groupby("Districts")["Transaction_amount"].sum().sort_values(ascending=True)
     dt2= pd.DataFrame(dt1).reset_index().head(50)
 
@@ -331,67 +319,18 @@ def ques10():
                 color_discrete_sequence= px.colors.sequential.Mint_r)
     return st.plotly_chart(fig_dt)
 
-
-st.set_page_config(layout= "wide")
-
-st.title("PHONEPE DATA VISUALIZATION AND EXPLORATION")
+st.set_page_config(page_title="PhonePe Pulse Data Visualization and Exploration", page_icon=":dragon:", layout="wide", initial_sidebar_state="auto")
+st.markdown("<h1 style='text-align: center; color: violet;'>PHONEPE PULSE DATA VISUALIZATION AND EXPLORATION</h1>", unsafe_allow_html=True)
 tab1, tab2, tab3 = st.tabs(["***HOME***","***EXPLORE DATA***","***TOP CHARTS***"])
 
 with tab1:
-    col1,col2= st.columns(2)
-
-    with col1:
-        st.header("PHONEPE")
-        st.subheader("INDIA'S BEST TRANSACTION APP")
-        st.markdown("PhonePe  is an Indian digital payments and financial technology company")
-        st.write("****FEATURES****")
-        st.write("   **-> Credit & Debit card linking**")
-        st.write("   **-> Bank Balance check**")
-        st.write("   **->Money Storage**")
-        st.write("   **->PIN Authorization**")
-        st.download_button("DOWNLOAD THE APP NOW", "https://www.phonepe.com/app-download/")
-    with col2:
-        st.write(" Phonepe")
-
-
-    col3,col4= st.columns(2)
-    
-    with col3:
-        st.write(" Phonepe")
-
-    with col4:
-        st.write("**-> Easy Transactions**")
-        st.write("**-> One App For All Your Payments**")
-        st.write("**-> Your Bank Account Is All You Need**")
-        st.write("**-> Multiple Payment Modes**")
-        st.write("**-> PhonePe Merchants**")
-        st.write("**-> Multiple Ways To Pay**")
-        st.write("**-> 1.Direct Transfer & More**")
-        st.write("**-> 2.QR Code**")
-        st.write("**-> Earn Great Rewards**")
-
-    col5,col6= st.columns(2)
-
-    with col5:
-        st.markdown(" ")
-        st.markdown(" ")
-        st.markdown(" ")
-        st.markdown(" ")
-        st.markdown(" ")
-        st.markdown(" ")
-        st.markdown(" ")
-        st.markdown(" ")
-        st.markdown(" ")
-        st.write("**->No Wallet Top-Up Required**")
-        st.write("**->Pay Directly From Any Bank To Any Bank A/C**")
-        st.write("**->Instantly & Free**")
-
-    with col6:
-        st.write(" Phonepe")
-        
+    st.header("PHONEPE")
+    st.subheader("INDIA'S BEST TRANSACTION APP")
+    st.markdown("PhonePe  is an Indian digital payments and financial technology company")
+    st.download_button("DOWNLOAD THE APP NOW", "https://www.phonepe.com/app-download/")
 
 with tab2:
-    sel_year = st.selectbox("select the Year",("All", "2018", "2019", "2020", "2021", "2022", "2023"))
+    sel_year = st.selectbox("Select Year",("All", "2019", "2020", "2021", "2022", "2023"))
     if sel_year == "All" :
         col1, col2 = st.columns(2)
         with col1:
@@ -402,7 +341,7 @@ with tab2:
             animate_all_count()
             payment_amount()
 
-        state=st.selectbox("selecet the state",('Andaman & Nicobar', 'Andhra Pradesh', 'Arunachal Pradesh',
+        state=st.selectbox("Select State",('Andaman & Nicobar', 'Andhra Pradesh', 'Arunachal Pradesh',
                                                 'Assam', 'Bihar', 'Chandigarh', 'Chhattisgarh',
                                                 'Dadra and Nagar Haveli and Daman and Diu', 'Delhi', 'Goa',
                                                 'Gujarat', 'Haryana', 'Himachal Pradesh', 'Jammu & Kashmir',
@@ -423,7 +362,7 @@ with tab2:
         with col2:
             transaction_count_year(sel_year)
             payment_amount_year(sel_year)
-            state= st.selectbox("selecet the state",('Andaman & Nicobar', 'Andhra Pradesh', 'Arunachal Pradesh',
+            state= st.selectbox("Select State",('Andaman & Nicobar', 'Andhra Pradesh', 'Arunachal Pradesh',
                                                 'Assam', 'Bihar', 'Chandigarh', 'Chhattisgarh',
                                                 'Dadra and Nagar Haveli and Daman and Diu', 'Delhi', 'Goa',
                                                 'Gujarat', 'Haryana', 'Himachal Pradesh', 'Jammu & Kashmir',
